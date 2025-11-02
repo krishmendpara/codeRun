a6ca8ae2f9daf285f234f55574cc6cee5b4ff6af
krishmendpara <krishmendpara5@gmail.com>
2025-11-02 16:43:11 +0530

    Added new origin

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,13 @@ import codeRoute from './routes/code.route.js'
 const app = express();
 
 connect();
-const allowedOrigins = [
-    'http://localhost:4173',  
-    'https://code-run-nine.vercel.app',
-    'https://coderunapp.vercel.app'  
-];
 app.use(cors({
-  origin: true,  // Allows ALL origins
+  origin: [
+    'http://localhost:4173',
+    'http://localhost:5173',
+    'https://code-run-nine.vercel.app',
+    'https://coderunapp.vercel.app'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
